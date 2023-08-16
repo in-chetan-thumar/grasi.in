f5cfd6311dc4cf723220c11b5601123e99ca610f
Abhishek Rajput <abhisek.khambhara@quantuminfoway.com>
2023-08-16 18:15:33 +0530

    llumar Dealer

--- www/app/Http/Controllers/frontend/locatetController.php
+++ www/app/Http/Controllers/frontend/locatetController.php
@@ -63,9 +63,8 @@ class LocatetController extends Controller
        public function getFilteredData(Request $request) {
             $filter = $request->city;
             $DealerData = $this->repository->filterData('city',$filter);
-            // return view('admin.user.llumarDealers', compact('DealerData'))->render();
             return response()->json($DealerData);
-            // return view('frontend.locate', compact('DealerData','llmarDealerData'))->render();
+           ;
            
         
 
