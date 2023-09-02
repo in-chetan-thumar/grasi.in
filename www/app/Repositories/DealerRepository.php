bb3408bb941e31656fd7ffaf3afdee222040a13e
in-chetan-thumar <chetan.thumar@tiez.nl>
2023-09-02 17:10:28 +0530

    Fix locate issue

--- www/app/Repositories/DealerRepository.php
+++ www/app/Repositories/DealerRepository.php
@@ -17,24 +17,24 @@ class DealerRepository
     {
         if($param == 'state')
         {
-            return $this->model->select('state')->groupBy('state') ->pluck('state');
+            return $this->model->select('state')->where('is_active', 'Y')->groupBy('state')->pluck('state');
         }
 
         if($param == 'city')
         {
             if(!empty($state))
             {
-                return $this->model->select('city')->where('state',$state)->groupBy('city') ->pluck('city');
+                return $this->model->select('city')->where('is_active', 'Y')->where('state',$state)->groupBy('city') ->pluck('city');
 
             }else{
 
-                return $this->model->select('city')->groupBy('city') ->pluck('city');
+                return $this->model->select('city')->where('is_active', 'Y')->groupBy('city') ->pluck('city');
             }
         }
 
         if($param == 'all')
         {
-            return $this->model->get();
+            return $this->model->where('is_active', 'Y')->get();
         }
 
 
@@ -78,7 +78,7 @@ class DealerRepository
     {
 
         if ($param === 'city') {
-           return $this->model->where('city',$filter)->get();
+           return $this->model->where('is_active', 'Y')->where('city',$filter)->get();
         }
     }
 }
