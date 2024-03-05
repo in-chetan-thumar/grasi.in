09e0d501f872cee88333fcd429a979b529524407
vikram shah <89067906+vikramshah1016@users.noreply.github.com>
2024-03-05 11:46:23 +0530

    Add Google tag (gtag.js)

--- www/resources/views/landing_page/thank-you.blade.php
+++ www/resources/views/landing_page/thank-you.blade.php
@@ -64,7 +64,17 @@
             src="https://www.facebook.com/tr?id=435501495476111&ev=PageView&noscript=1" /></noscript>
     <!-- End Meta Pixel Code -->
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11116955619"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'AW-11116955619');
+    </script>
 </head>
 
 <body>
